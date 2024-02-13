56d2f66c024195ae84377303cc3c7dd27d1e86d5
abdirashid haji <abdirashid.ke@gmail.com>
2024-02-13 21:47:41 +0300

    Updates on

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Smooth scrolling for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+  // Smooth scrolling for anchor links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-        });
-
-        // Close the mobile menu if it's open
-        document.getElementById('mobileMenuButton').classList.remove('open');
-        document.getElementById('mobileMenu').classList.add('hidden');
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
       });
-    });
 
-    // Toggle mobile menu visibility
-    document.getElementById('mobileMenuButton').addEventListener('click', function () {
-      const mobileMenu = document.getElementById('mobileMenu');
-      this.classList.toggle('open');
-      mobileMenu.classList.toggle('hidden');
+      // Close the mobile menu if it's open
+      document.getElementById('mobileMenuButton').classList.remove('open');
+      document.getElementById('mobileMenu').classList.add('hidden');
     });
   });
+
+  // Toggle mobile menu visibility
+  document.getElementById('mobileMenuButton').addEventListener('click', function () {
+    const mobileMenu = document.getElementById('mobileMenu');
+    this.classList.toggle('open');
+    mobileMenu.classList.toggle('hidden');
+  });
+});
