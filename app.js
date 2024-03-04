97ca639dc7d7bd681cc4f4b03d7f58a7d83348ec
abdirashid haji <abdirashid.ke@gmail.com>
2024-03-04 18:35:43 +0300

    remove items

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Smooth scrolling for anchor links
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -8,13 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
         behavior: 'smooth'
       });
 
-      // Close the mobile menu if it's open
       document.getElementById('mobileMenuButton').classList.remove('open');
       document.getElementById('mobileMenu').classList.add('hidden');
     });
   });
 
-  // Toggle mobile menu visibility
   document.getElementById('mobileMenuButton').addEventListener('click', function () {
     const mobileMenu = document.getElementById('mobileMenu');
     this.classList.toggle('open');
